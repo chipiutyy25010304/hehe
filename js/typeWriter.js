059d73149d3fe64b2712f7b7bc54b60a266b81fe
chipiutyy25010304 <phuongkv1975@gmail.com>
2026-04-07 21:37:31 +0700

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey! Miss Tyy.";
+let text2 = "You are my sunshine."
 let speed = 100;
 
 function typeWriter(text, para){
